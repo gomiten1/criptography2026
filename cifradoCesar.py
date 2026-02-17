20278c623220cdfd44d77af3140ce246eedca2bb
cc <111659435+c-barco@users.noreply.github.com>
2026-02-17 12:18:52 -0600

    descifrado

--- cifradoCesar.py
+++ cifradoCesar.py
@@ -14,8 +14,26 @@ def cesar(mensaje: str, k: int) -> str:
             resultado += chr((ord(caracter) - ord('a') + k) % 26 + ord('a'))
     return resultado
 
+def descifrar(mensaje: str, k: int) -> str:
+    """Descifra un mensaje utilizando el cifrado César.
+    Devuelve un mensaje en minúsculas, con espacios y sin caracteres especiales."""
+    
+    resultado = ""
+    for caracter in mensaje:
+        if caracter == " ":
+            resultado += " "
+        elif not caracter.isalpha():
+            resultado += caracter
+        elif caracter.isupper():
+            resultado += chr((ord(caracter) - ord('A') - k) % 26 + ord('A'))
+        else:
+            resultado += chr((ord(caracter) - ord('a') - k) % 26 + ord('a'))
+    return resultado
+
 if __name__ == "__main__":
     mensaje = input("Ingrese el mensaje a cifrar: ")
     k = int(input("Ingrese el número de posiciones para el desplazamiento: "))
     mensaje_cifrado = cesar(mensaje, k)
     print(f"Mensaje cifrado: {mensaje_cifrado}")
+    mensaje_descifrado = descifrar(mensaje_cifrado, k)
+    print(f"Mensaje descifrado: {mensaje_descifrado}")
